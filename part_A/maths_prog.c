a33df5504465533051b90c3f7cebb26b4f06e32a
lukezsmith <lukezachsmith@gmail.com>
2020-12-03 17:47:15 +0000

    part A completed

---
+++ part_A/maths_prog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include "subtract.h"
+#include "add.h"
+
+#define MY_LIB "libmaths.so"
+
+
+int main(){
+  int a,b;
+  scanf("%d %d",&a,&b);
+  printf("%d+%d=%d\n",a,b,add(a,b)); 
+  printf("%d-%d=%d\n",a,b,subtract(a,b)); 
+  return 0;
+}
