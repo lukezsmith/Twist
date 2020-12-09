42b4443560170985b5649fa6a6404732ab86e43d
lukezsmith <lukezachsmith@gmail.com>
2020-12-09 18:46:02 +0000

    part D implemented, just testing required

--- part_D/sort.c
+++ part_D/sort.c
@@ -22,6 +22,15 @@ enum { MAXL = 40, MAXC = 50 };
 
 // must use switch case to choose arguments. 
 
+void displayUsageInfo(){
+  printf("This is the usage for sort \n"
+       "Valid options are:\n"
+       "    --r: reverse the sorting order\n"
+       "    --o: specify output file for the sorted file to be saved to\n"
+       "    --n: use numerical value to sort on\n"
+       "    --h: display usage information the program\n");
+}
+
 int cmpfunc (const void * a, const void * b) {
   const char* aa = (const char*)a;
   const char* bb = (const char*)b;
@@ -82,8 +91,21 @@ int reversenumbercmpfunc(const void * a, const void * b) {
   return ( (int)retb - (int)reta );
 } 
 
+
+void writeToFile(char *filename, char *lines, int n){
+  printf("writing......!");
+  int i = 0;
+  FILE *f;
+  // check file 
+  if ((f = fopen(filename, "wb")) == NULL) {
+    printf("Can't open file %s\n", filename);
+  }
+  for (i = 0; i < n; i++) fprintf (f, "%c\n", lines[i]);
+  fclose(f);
+}
 int main(int argc, char *argv[]){
   char (*lines)[MAXC] = NULL; 
+  char (*outputFilename) = "";
   char sortType = 'd';
   char outputType = 'c';
   int reverse = 0;
@@ -96,21 +118,25 @@ int main(int argc, char *argv[]){
       switch ((*argv)[1]){
         
         default: 
-          printf("Unknown option-%c\n\n", (*argv)[1]);
+          printf("Unknown option -%c\n\n", (*argv)[1]);
+          displayUsageInfo();
           return 1;
         
         case 'h': 
           // if file specified, process & sort using default
           // display usage info
-          // displayUsageInfo();
-
-          break;
+          displayUsageInfo();
+          return 1;
 
         case 'o': 
           outputType = 'f';
-          // process & sort
+          // get filename 
+          char* filename = (*++argv);
+          printf("output filename: %s\n", filename);
 
-          // write to specified file (if possible, else create new file and output to there????)
+          // set filename 
+          outputFilename = filename;
+          // outputFilename = "TETS";
           break;          
         
         case 'n':
@@ -138,12 +164,12 @@ int main(int argc, char *argv[]){
     return 1;
   }else{
     // process file
-    ProcessFileAlt(fp, argv[-1], sortType, outputType, reverse);
+    ProcessFileAlt(fp, argv[-1], sortType, outputType, reverse, outputFilename);
     return 0;
     }
 }
 
-void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, int reverse){
+void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, int reverse, char* outputFilename){
   char (*lines)[MAXC] = NULL; /* pointer to array of type char [MAXC] */
   
   int i, n = 0, maxl = MAXL;
@@ -175,8 +201,8 @@ void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, in
   if (fp != stdin) fclose (fp);   /* close file if not stdin */
 
   /* print lines */
-  printf("Original array: \n");
-  for (i = 0; i < n; i++) printf (" line[%2d] : '%s'\n", i + 1, lines[i]);
+  // printf("Original array: \n");
+  // for (i = 0; i < n; i++) printf ("%s\n", lines[i]);
 
   free (lines);   /* free allocated memory */
 
@@ -208,9 +234,19 @@ void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, in
   // if file output
   if(outputType == 'f'){
     // file output logic
+    // writeToFile(outputFilename, lines, n);
+    printf("writing......!");
+    // int i = 0;
+    FILE *f;
+    // check file 
+    if ((f = fopen(outputFilename, "wb")) == NULL) {
+      printf("Can't open file %s\n", outputFilename);
+    }
+    for (i = 0; i < n; i++) fprintf (f, "%s\n", lines[i]);
+    fclose(f);
   }else{
     /* print lines */
     printf("Sorted array: \n");
-    for (i = 0; i < n; i++) printf (" line[%2d] : '%s'\n", i + 1, lines[i]);
+    for (i = 0; i < n; i++) printf ("%s\n", lines[i]);
   }
 }
--- part_D/sort.h
+++ part_D/sort.h
@@ -1,2 +1,3 @@
-void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, int reverse);
+void ProcessFileAlt(FILE* fp, char* filename, char sortType, char outputType, int reverse, char* outputFilename);
 void ProcessFile(FILE* fp, char* filename);
+void writeToFile(char* filename, char* lines, int n);
