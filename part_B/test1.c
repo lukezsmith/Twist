da856ca1bd0ae913faa77f818fe4902a9a23f61d
lukezsmith <lukezachsmith@gmail.com>
2021-01-11 22:34:37 +0000

    test script refreshed

--- part_B/test1.c
+++ part_B/test1.c
@@ -12,13 +12,11 @@ int main(){
   next_move.column=3;
   next_move.row=4;
   play_move(next_move,my_board);
-  // outfile=fopen("test_output1.txt","w");
-  // write_out_file(outfile,my_board);
   next_move.column=2;
   next_move.row=2;
   play_move(next_move,my_board);
 
-  outfile=fopen("test_output2.txt","w");
+  outfile=fopen("test_output1.txt","w");
   write_out_file(outfile,my_board);
   fclose(outfile);
 
